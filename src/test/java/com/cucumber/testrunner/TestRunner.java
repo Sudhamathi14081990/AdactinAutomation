83a167c9ac28175450527aec88e20739fae2b018
Sudhamathi14081990 <r.n.sudhamathi@gmail.com>
2019-09-12 09:29:09 +0530

    U[pdated test runner with cucumber optiond

--- src/test/java/com/cucumber/testrunner/TestRunner.java
+++ src/test/java/com/cucumber/testrunner/TestRunner.java
@@ -1,32 +1,41 @@
- package com.cucumber.testrunner;
+package com.cucumber.testrunner;
+
+import java.io.File;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
 
 import com.cucumber.baseclass.BaseClass;
+import com.cucumberpractice.properties.FileDataManager;
 
 import cucumber.api.CucumberOptions;
+
 import cucumber.api.junit.Cucumber;
+import com.cucumber.listener.Reporter;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-		features="src\\test\\java\\com\\cucumber\\feature", glue="com.cucumber.stepdefinition"
-		)
-		// monochrome=true, strict= true,dryRun=false
-		
+@CucumberOptions(plugin= {"com.cucumber.listener.ExtentCucumberFormatter:src\\test\\resource\\com\\cucumber\\reports\\extentreports\\extentreport.html", 
+		"pretty", "json:src\\test\\resource\\com\\cucumber\\reports\\report.json", "junit:src\\test\\resource\\com\\cucumber\\reports\\report.xml", "html:src\\test\\resource\\com\\cucumber\\reports"},
 
+		features = "src\\test\\java\\com\\cucumber\\featureFile\\MakeMyTrip.feature", glue = "com.cucumber.stepdefinition", dryRun = false, strict=true, monochrome=true, tags = "@testfeature")
 public class TestRunner extends BaseClass {
-@BeforeClass
-public static void beforeclass() throws Exception {
-System.out.println("Driver Initialization");
-driver=getBrowser("Chrome");
-}
-@AfterClass
-public static void afterclass() {
-	System.out.println("Driver Quit");
-	driverQuit(driver);
+	public static WebDriver driver;
+	@BeforeClass
+	
+	public static void beforeclass() throws Throwable {
+		System.out.println("Driver Initialization");
+		String browserName = FileDataManager.fdm.getCr().getBrowserName();
+		driver = getBrowser(browserName);
 
-}
+	}
 
+	@AfterClass
+	public static void afterclass() {
+		System.out.println("Driver Quit");
+		// driverQuit(driver);
+		Reporter.loadXMLConfig(new File(System.getProperty("user.dir") + "\\src\\test\\java\\com\\cucumberpractice\\properties\\extent-config.xml"));
+
+	}
 }
